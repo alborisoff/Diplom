034f5fd9b2d9e3bf5466bdd9ce688d8f95c770fd
aleks <alekseyaborisoff@gmail.com>
2016-12-04 10:06:28 -0800

    Добавлена функция пересчёта прямоугольных координат по формуле Гельмерта.

--- coordtransform.py
+++ coordtransform.py
@@ -4,12 +4,6 @@ import yaml as ym
 import numpy as np
 
 
-class Cfg:
-
-    def __init__(self):
-        pass
-
-
 class Srv:
 
     def __init__(self):
@@ -24,7 +18,6 @@ class Srv:
         mins = dms[3:5]
         secs = dms[6:len(dms)]
         degrees = float(degs) + float(mins) / 60 + float(secs) / 3600
-        print 'DMS: ', degs, ':', mins, ":", secs
         return degrees
 
     def ellipsoid_params(self, ellipsoid_name):
@@ -32,7 +25,48 @@ class Srv:
         elldict = {}
         if ellipsoid_name in ell_library:
             elldict['a'] = ell_library[ellipsoid_name]['a']
-            elldict['alpha'] = 1/ell_library[ellipsoid_name]['ralpha']
+            elldict['ralpha'] = ell_library[ellipsoid_name]['ralpha']
+        return elldict
 
-    def transform_params(self, system_old, system_new):
+class Transform:  # Класс для трансформирования координат
+
+    def __init__(self):
         pass
+
+    def blh2xyz(self, B, L, H, ellipsoid_name):  # Пересчёт эллипсоидальных координат B, L, H в геоцентрические X, Y, Z
+        # Входные данные должны быть представлены в виде массива [B, L, H] - геодезические широта, долгота и высота.
+        # Широта и долгота в виде ггг.гггггг (т.е. градусы и доли градуса), высота в метрах и долях метра.
+        # Получаем параметры эллипсоида
+        a = float(Srv().ellipsoid_params(ellipsoid_name)['a'])  # Большая полуось
+        alpha = 1/float(Srv().ellipsoid_params(ellipsoid_name)['ralpha'])  # Сжатие эллипсоида
+        # Синус и косинус широты и долготы
+        sinB = np.sin(np.radians(B))
+        sinL = np.sin(np.radians(L))
+        cosB = np.cos(np.radians(B))
+        cosL = np.cos(np.radians(L))
+        sinsqB = sinB**2  # Квадрат синуса широты
+        esq = 2*alpha - alpha**2  # Квадрат первого эксцентриситета
+        subradical = 1 - esq*sinsqB  # Подкоренное значение для вычисления радиуса кривизны первого вертикала
+        N = a/(subradical ** 0.5)  # Вычисление радиуса первого вертикала
+        # Вычисление X, Y, Z
+        X = (N + H) * cosB * cosL
+        Y = (N + H) * cosB * sinL
+        Z = (H + N) * sinB - esq * N * sinB
+        # Выдача результата в виде массива [X, Y, Z]
+        return [X, Y, Z]
+
+
+
+ellipsoid_name = 'WGS84'
+B_dms = '55 51 6.47082'
+L_dms = '37 26 11.16388'
+H = 189.000
+B = Srv().dms2ddd(B_dms)
+L = Srv().dms2ddd(L_dms)
+print Transform().blh2xyz(B, L, H, ellipsoid_name)
+
+
+
+
+
+
